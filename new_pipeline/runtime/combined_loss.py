4c28e795df6668e850a002abdca897785b5502d0
mohamedsafy <mohamedsafy>
2026-06-03 21:03:07 +0000

    m

--- new_pipeline/runtime/combined_loss.py
+++ new_pipeline/runtime/combined_loss.py
@@ -37,7 +37,7 @@ class CombinedLoss(nn.Module):
             # Check if it's a standard NN loss (like CE) or an SMP loss
             if isinstance(fn, (nn.CrossEntropyLoss, nn.NLLLoss)):
                 target_sq = target_sq.long()  # Ensure targets are long for CE losss
-                pred_sq = pred_sq.long()  # Ensure predictions are long for loss calculation
+                pred_sq = pred_sq.float()  # Ensure predictions are long for loss calculation
                 loss += fn(pred_sq, target_sq) * weight
             else:
                 loss += fn(pred_sq, target) * weight
