0fbfc5a049cbe6c265d40a2462e7e0a3efb088db
Mohamed <mohammadsafyyt@gmail.com>
2026-06-11 05:38:46 +0000

    m

--- new_pipeline/runtime/data_module.py
+++ new_pipeline/runtime/data_module.py
@@ -9,17 +9,26 @@ class DataModule:
     train_ds: Any
     val_ds: Any
     test_ds: Any
+    _train_loader: Any = None
+    _val_loader: Any = None
+    _test_loader: Any = None
     batch_size: int = 1
     num_workers: int = 0
 
     def train_loader(self) -> Any:
-        return self._build_loader(self.train_ds, shuffle=True)
+        if self._train_loader is None:
+            self._train_loader = self._build_loader(self.train_ds, shuffle=True)
+        return self._train_loader
 
     def val_loader(self) -> Any:
-        return self._build_loader(self.val_ds, shuffle=False)
+        if self._val_loader is None:
+            self._val_loader = self._build_loader(self.val_ds, shuffle=False)
+        return self._val_loader
 
     def test_loader(self) -> Any:
-        return self._build_loader(self.test_ds, shuffle=False)
+        if self._test_loader is None:
+            self._test_loader = self._build_loader(self.test_ds, shuffle=False)
+        return self._test_loader
 
     def _build_loader(self, dataset: Any, shuffle: bool) -> Any:
         try:
